7c0f96a7eee870d11a81f65e68369acfe269383c
John Martin <john.itvn@gmail.com>
2023-01-18 02:36:06 +0700

    chore: draft profile image component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {ProfileImageComponent} from './components/profile-image/profile-image.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, ProfileImageComponent],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
